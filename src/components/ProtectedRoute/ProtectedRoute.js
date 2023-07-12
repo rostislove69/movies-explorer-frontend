a6ef00e183350ad4d0c846f10fb881f1aff4729f
Garbuz Rostislav <rostislavgarbuz@yandex.ru>
2023-07-12 21:56:33 +0300

    fix: при попытке перейти на любой защищённый роут происходит редирект на /

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouteElement = ({element: Component, ...props}) => {
-  return props.loggedIn ? <Component {...props} /> : <Navigate to="/signin"/>
+  return props.loggedIn ? <Component {...props} /> : <Navigate to="/"/>
 }
 
 export default ProtectedRouteElement;
